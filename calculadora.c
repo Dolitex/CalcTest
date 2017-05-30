0bd576c4874e65c2073fa901f5c27c83b86ae2c8
Rian Reis <contato@rianreis.com.br>
2017-05-30 16:11:59 -0300

    Função de adição implementada

--- calculadora.c
+++ calculadora.c
@@ -1,8 +1,25 @@
-#include <sdtio.h>
+#include <stdio.h>
 
 int main()
 {
+	char comando;
+
 	printf("A calculadora está pronta para uso!\n");
+
+	printf("Por favor digite o sinal respectivo da operação que você deseja fazer.\n");
+
+	scanf("%c", &comando);
+
+	if(comando == '+')
+	{
+		int n1,n2;
+		printf("Digite os números para a soma.\n");
+		scanf("%d %d", &n1, &n2);
+
+		int result = n1 + n2;
+
+		printf("O resultado da soma é %d", result);
+	}
 
 
 	return 0;
